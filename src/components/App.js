24b01f4f8501d5fc052951ec09d6ea03f8d99432
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-18 11:39:15 +0530

    use e.detail

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
+let timer;
 const App = () => {
 
-  const handleDoubleClick = (event) =>{
-  
+  const handleDoubleClick = (e) =>{
+    // if(timer){
+    //   clearTimeout(timer);
+    //   return;
+    // }
+    // const timer = setTimeout(()=>{
+      if(e.detail === 2){
+        console.log("I was double clicked");
+      }else{
+        console.log("I was not double clicked");
+      }
+    // },500);
+    
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn"  onClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
